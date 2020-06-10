fd948a9594f84a8a68ee4dc8f6850c0a9e23d4e4
hanwensheng <hanwensheng@lionbridgecapital.cn>
2020-06-10 11:00:23 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        hello next12
+        <hr />
+        <Button type="primary">xxxx</Button>
+      </div>
+    </>
+  );
+}
